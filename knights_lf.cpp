65fae4c134257b7e5eacaa7fe42580f276e8838a
iawflower <iawflower@gmail.com>
2013-11-18 16:41:33 -0800

    Updated knights_lf to fix a bug where possible moves weren't updating properly.

--- knights_lf.cpp
+++ knights_lf.cpp
@@ -3,17 +3,35 @@
 #include <iostream>
 using namespace std;
 
+/*
+
+This program's purpose is to find Knight's Tours by employing a "least freedom" algorithm. It works by emulating a knight as it attempts this tour, prompting the knight to move to spaces from which it will have the least freedom first. This method causes the Knight to preferentially seek out the "difficult to reach" sections, which improves the speed at which a knight's tour can be found. In fact, knight's tours can frequently be found on the first try using this algorithm.
+
+The general form of this program is to keep track of the spaces and moves in a series of arrays. The current location of the piece is kept in an integer variable called "here", which represents the location in the array of that space. The currently traced path is held in the "lasts" array, in which each space's entry is the integer location of the previous space visited.
+
+The possible moves out of a space are stored in a 2-D aray called "possibles", in which there is an array of maximum size 8 for each space on the board. A separate array called ipossibles records the number of remaining moves out of a space, to facilitate the least freedom algorithm. The routes already taken are stored in the travelled and itravelled arrays in a similar fashion. The array "visisted" keeps track of whether or not a space has been visited so far.
+
+Finally, a counter is maintained to keep track of progress. When the counter reaches the number of spaces on the board, a knight's tour has been found.
+
+*/
+
+
+
 #define MAXSIZE 25
 #define MINSIZE 6
 
-void initialize();
-int findnext();
-bool intravelled (int source, int target);
-void visit (int loc);
-void removepossible (int source, int target);
-void backtrack();
-void printResult();
-void printAll();
+
+//////
+//	Function declarations.
+//////
+void initialize();									// Creates the board and moves.
+int findnext();										// Executes the least freedom algorithm by determining which space to move to next.
+bool intravelled (int source, int target);			// Checks to see if the target has been travelled to from the source.
+void visit (int loc);								// Visits a space, changing all the values required.
+void removepossible (int source, int target);		// Removes source from target's list of possible moves.
+void backtrack();									// Moves backward in the knight's tour, called when the algorithm hits a dead end.
+void printResult();									// Prints the knight's tour in order of visitation.
+void printAll();									// Prints the knight's tour by space number.
 
 int here;
 int n;
@@ -66,14 +84,14 @@ int main () {
 	
 }
 
-int findnext() {
-	int tempnext = -1;
-	int tempi = 9;
-	for (int i = 0; i < ipossibles[here]; i++) {
-		cout << "\n\tTesting " << possibles[here][i];
-		int testcase = possibles[here][i];
-		if (ipossibles[testcase] < tempi) {
-			if (!(intravelled(here,testcase))) {
+int findnext() { 					// Executes the least freedom algorithm by determining which space to move to next.
+	int tempnext = -1;				// Tempnext/tempi represent the possible place to move to next.
+	int tempi = 9;					// Initially, it points to a non existent place with an absurd degree of freedom. (Max on a chess board is 8)
+	for (int i = 0; i < ipossibles[here]; i++) {	// then start scanning through the possibles of here
+		cout << "\n\tTesting " << possibles[here][i];	// print out for kicks
+		int testcase = possibles[here][i];			// And use the word testcase to simplify the code's appearance
+		if (ipossibles[testcase] < tempi) {		// if the testcase has less freedom, then that looks good
+			if (!(intravelled(here,testcase))) {	//
 				if (!(visited[testcase])) {
 					tempnext = testcase;
 					tempi = ipossibles[testcase];
@@ -84,7 +102,7 @@ int findnext() {
 	return tempnext;
 }
 
-bool intravelled(int source, int target) {
+bool intravelled(int source, int target) {		// Checks to see if the target has been travelled to from the source.
 	for (int i = 0; i < itravelled[source]; i++) {
 		if (travelled[source][i] == target) {
 			return true;
@@ -93,38 +111,37 @@ bool intravelled(int source, int target) {
 	return false;
 }
 
-void visit(int loc) {
-	in++;
-	cout << "\nf " << in << " , " << loc;
+void visit(int loc) {				// Visits a space, changing all the values required.
+	in++;		// Increment the number of spaces visited counter.
+	cout << "\nf " << in << " , " << loc; // Print out where you are for kicks.
 	
-	travelled[here][itravelled[here]] = loc;
+	travelled[here][itravelled[here]] = loc;	// Add here to the last place's travelled to list.
 	itravelled[here]++;
-	visited[loc] = true;
-	lasts[loc] = here;
-	here = loc;
-	int j = 0;
+	visited[loc] = true;		// set the visited for the new place to true
+	lasts[loc] = here;			// set the last place to this place's "lasts"
+	here = loc;					// then move forward
+	int j = 0;					// and remove the new place from all adjacent space's possibles lists.
 	for (int i = 0; i < ipossibles[here]; i++) {
 		removepossible(possibles[here][i],here);
 	}
 }
 
-void removepossible (int source, int target) {
+void removepossible (int source, int target) {			// Removes target from source's list of possible moves.
 	bool foundit = false;
-	for (int i = 0; i < (ipossibles[source]); i++) {
+	for (int i = 0; i < (ipossibles[source]); i++) {	// scan through the list looking for target,
 		if (foundit) {
-			possibles[source][i-1] = possibles[source][i];
-		};
-		if (possibles[source][i] == target) {
+			possibles[source][i-1] = possibles[source][i];	// once you've found it, start sliding values over.
+		} else if (possibles[source][i] == target) {		// and keep looking for it if you haven't.
 			foundit = true;
 		};
 	};
-	if (foundit) {
+	if (foundit) {		// And then handle the last element in the list.
 		ipossibles[source]--;
 		possibles[source][ipossibles[source]] = -1;
 	};
 }
 
-void backtrack() {
+void backtrack() {									// Moves backward in the knight's tour, called when the algorithm hits a dead end.
 	in--;
 	cout << "\nb " << in << " , " << lasts[here];
 	
@@ -148,26 +165,26 @@ void backtrack() {
 	here = prev;
 }
 
-void initialize () {
-	for (int i = 0; i < n; i++) {
+void initialize () {											// Creates the board and moves.
+	for (int i = 0; i < n; i++) {	// For each space on the board...
 		
-		lasts[i]=-1;
+		lasts[i]=-1;				// Initialize its previous space.
 		for (int k = 0; k < 8; k++) {
-			travelled[i][k] = -1;
+			travelled[i][k] = -1;	// Initialize its travelled to list.
 		}
 		itravelled[i] = 0;
-		visited[i] = false;
-		ipossibles[i] = 0;
+		visited[i] = false;			// Make it not yet visited.
+		ipossibles[i] = 0;			// Start with 0 possible moves then...
 		
-		int * j = &ipossibles[i];
-		int x = i % w;
+		int * j = &ipossibles[i];	// Using a pointer to the ipossibles counter,
+		int x = i % w;				// Find the x and y coordinates of the space,
 		int y = i / w;
 		for (int k = 0; k < 8; k++) {
-			possibles[i][k] = -1;
+			possibles[i][k] = -1;	// Initialize the set of possibles, then...
 		};
-		if ((x<(w-1))&&(y>1)) {
-			possibles[i][*j] = (i - (2*w) + 1);
-			(*j)++;
+		if ((x<(w-1))&&(y>1)) {		// Start adding all the possible moves by evaluating where on the board this space is.
+			possibles[i][*j] = (i - (2*w) + 1); // Check if a given space should have that move,
+			(*j)++;								// And add it, incrementing the ipossibles counter, if it does.
 		};
 		if ((x<(w-2))&&(y>0)) {
 			possibles[i][*j] = (i - w + 2);
@@ -197,7 +214,7 @@ void initialize () {
 			possibles[i][*j] = (i - (2*w) - 1);
 			(*j)++;
 		};
-		cout << "\nThe possibles from " << i << " are: ";
+		cout << "\nThe possibles from " << i << " are: "; // For kicks, print out the list of possibles for that space.
 		for (int k = 0; k < ipossibles[i]; k++) {
 			cout << possibles[i][k] << " ";
 		};
